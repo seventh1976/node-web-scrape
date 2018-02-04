cf00b0a23d7995a8fb648f23a5097ab34c901ac5
seventh1976 <sevenh1976@gmail.com>
2018-02-04 13:49:25 +0800

    traversing the DOM

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.get('/scrape', function(req, res){
 
         var title, release, rating;
         var json = {title : "", release : "", rating : ""}
+
+        $('.header').filter(function(){
+            var data = $(this);
+            title = data.children().first().text();
+            json.title = title;
+        })
     })
 })
 
